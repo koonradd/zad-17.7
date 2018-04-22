545abd26dd9748f3bd467e58c26fa707de98a958
Konrad Kościelski <konrad.koscielski@interia.pl>
2018-04-23 00:30:45 +0200

    init

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  'GOOGLE_CLIENT_ID':'1045908063384-8qrqrm5gcbe1bmt109s01goic6abkubb.apps.googleusercontent.com',
+  'GOOGLE_CLIENT_SECRET' : '4HdImUjJiIflNxeHkFUfvpUR',
+  'CALLBACK_URL' : 'http://localhost:3000/auth/google/callback'
+};
